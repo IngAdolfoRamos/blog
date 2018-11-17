3bd9625cbafc53014d6c1864048de808baffb657
IngAdolfoRamos <luadramo@gmail.com>
2018-11-17 14:13:48 -0600

    Creating the model and the migration for catgories.

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // this line allows us to use the format function
+    //protected $dates = ['created_at'];
+
+    //This function makes the relation between post and category so a post belongs to
+    // a catgory
+    public function catgory()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
